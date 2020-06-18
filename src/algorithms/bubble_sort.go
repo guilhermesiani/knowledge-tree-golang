f8f0252e10bb60e59d0941055eb53379f573d0dc
Guilherme Siani <guilherme@siani.com.br>
2020-06-18 20:55:41 -0300

    Implement bubble sort

--- src/algorithms/bubble_sort.go
+++ src/algorithms/bubble_sort.go
@@ -1,5 +1,17 @@
 package algorithms
 
 func bubbleSort(toSort [5]int) [5]int {
-	return [5]int{1,3,5,8,9}
+	sorted := toSort
+	for i := 0; i < len(toSort); i++ {
+		toSort := sorted
+		for j := 0; j < len(toSort); j++ {
+			if (len(toSort)-(i+1)) >= j && sorted[j] > sorted[len(toSort)-(i+1)] {
+				toSort = sorted;
+				sorted[len(toSort)-(i+1)] = sorted[j]
+				sorted[j] = toSort[len(toSort)-(i+1)]
+			}
+		}
+	}
+
+	return sorted
 }
