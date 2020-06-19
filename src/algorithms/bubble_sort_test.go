1903b56d1a50e2aed53e87a57b8295fb278591ba
Guilherme Siani <guilherme@siani.com.br>
2020-06-18 22:36:34 -0300

    Add more one test for bubble sort

--- src/algorithms/bubble_sort_test.go
+++ src/algorithms/bubble_sort_test.go
@@ -11,4 +11,9 @@ func TestBubbleSort(t *testing.T) {
 		[5]int{1,3,5,8,9},
 		bubbleSort([5]int{8,5,3,9,1}),
 	)
+	assert.Equal(
+		t,
+		[5]int{-3,1,5,7,8},
+		bubbleSort([5]int{5,8,1,7,-3}),
+	)
 }
