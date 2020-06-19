048b4e93185637f93ceda53a44b4ca5bbd9b43aa
Guilherme Siani <guilherme@siani.com.br>
2020-06-18 22:49:08 -0300

    Simple refactor on Bubble Sort

--- src/algorithms/bubble_sort.go
+++ src/algorithms/bubble_sort.go
@@ -5,10 +5,11 @@ func bubbleSort(toSort [5]int) [5]int {
 	for i := 0; i < len(toSort); i++ {
 		toSort := sorted
 		for j := 0; j < len(toSort); j++ {
-			if (len(toSort)-(i+1)) >= j && sorted[j] > sorted[len(toSort)-(i+1)] {
+			pos := len(toSort)-(i+1)
+			if pos >= j && sorted[j] > sorted[pos] {
 				toSort = sorted;
-				sorted[len(toSort)-(i+1)] = sorted[j]
-				sorted[j] = toSort[len(toSort)-(i+1)]
+				sorted[pos] = sorted[j]
+				sorted[j] = toSort[pos]
 			}
 		}
 	}
