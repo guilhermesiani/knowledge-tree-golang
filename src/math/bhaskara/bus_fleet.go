b389e546b3b89909fe878542c408757f31c535b2
Guilherme Siani <guilherme@siani.com.br>
2020-07-24 21:20:03 -0300

    Implement bhaskara calc example

--- src/math/bhaskara/bus_fleet.go
+++ src/math/bhaskara/bus_fleet.go
@@ -1,13 +1,34 @@
 package bhaskara
 
-func calcMaxProfit(
+import "math"
+
+func CalcMaxProfit(
 	priceByPassenger float64,
 	startDiscount int,
 	discount float64,
 ) int {
-	// @TODO
+	// Find coefficients
+	a := discount * -1
+	b := priceByPassenger + (discount * float64(startDiscount))
 
-	//firstTerm := priceByPassenger + (discount * float64(startDiscount))
+	// Apply Bhaskara method
+	x1, x2 := applyBhaskara(a, b)
 
-	return 55
+	return int((x1 + x2) / 2)
+}
+
+func applyBhaskara(a, b float64) (float64, float64) {
+	c := make(chan float64)
+	go bhaskaraFormuleByOp(a, b, "-", c)
+	go bhaskaraFormuleByOp(a, b, "+", c)
+	return <-c, <-c
+}
+
+func bhaskaraFormuleByOp(a, b float64, op string, c chan float64) {
+	divisor := 2 * a
+	sqrtDelta := math.Sqrt(math.Pow(b, 2))
+	if op == "+" {
+		c <- (-b + sqrtDelta) / divisor
+	}
+	c <- (-b - sqrtDelta) / divisor
 }
--- src/math/bhaskara/bus_fleet_test.go
+++ src/math/bhaskara/bus_fleet_test.go
@@ -6,13 +6,6 @@ import (
 )
 
 func TestCalcMaxProfit(t *testing.T) {
-	assert.Equal(
-		t,
-		55,
-		calcMaxProfit(
-			350,
-			40,
-			5,
-		),
-	)
+	assert.Equal(t, 55, CalcMaxProfit(350, 40, 5))
+	assert.Equal(t, 60, CalcMaxProfit(350, 50, 5))
 }
