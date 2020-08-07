bb5fd653212d94e780af413b7956ea834a3c69f1
Guilherme Siani <guilherme@siani.com.br>
2020-08-06 21:26:00 -0300

    Bhaskara method refactored

--- src/math/bhaskara/bus_fleet.go
+++ src/math/bhaskara/bus_fleet.go
@@ -7,24 +7,30 @@ func CalcMaxProfit(
 	startDiscount int,
 	discount float64,
 ) int {
-	// Find coefficients
-	a := discount * -1
-	b := priceByPassenger + (discount * float64(startDiscount))
-
-	// Apply Bhaskara method
-	x1, x2 := applyBhaskara(a, b)
+	a, b := findCoefficients(priceByPassenger, startDiscount, discount)
+	x1, x2 := applyQuadraticEquation(a, b)
 
 	return int((x1 + x2) / 2)
 }
 
-func applyBhaskara(a, b float64) (float64, float64) {
+func findCoefficients(
+	priceByPassenger float64,
+	startDiscount int,
+	discount float64,
+) (float64, float64) {
+	a := discount * -1
+	b := priceByPassenger + (discount * float64(startDiscount))
+	return a, b
+}
+
+func applyQuadraticEquation(a, b float64) (float64, float64) {
 	c := make(chan float64)
-	go bhaskaraFormuleByOp(a, b, "-", c)
-	go bhaskaraFormuleByOp(a, b, "+", c)
+	go quadraticEquationFormulaByOp(a, b, "-", c)
+	go quadraticEquationFormulaByOp(a, b, "+", c)
 	return <-c, <-c
 }
 
-func bhaskaraFormuleByOp(a, b float64, op string, c chan float64) {
+func quadraticEquationFormulaByOp(a, b float64, op string, c chan float64) {
 	divisor := 2 * a
 	sqrtDelta := math.Sqrt(math.Pow(b, 2))
 	if op == "+" {
