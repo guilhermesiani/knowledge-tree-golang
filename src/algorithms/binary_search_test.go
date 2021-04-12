b870b91ed73869cdc81086c457d16252300195e0
Guilherme Siani <contact@guilhermesiani.com>
2021-04-12 08:17:36 -0300

    fix: binary search with wrong algorithm

--- src/algorithms/binary_search_test.go
+++ src/algorithms/binary_search_test.go
@@ -10,6 +10,11 @@ func TestBinarySearchSuccess(t *testing.T) {
 	assert.Equal(t, 2, binarySearchResult)
 }
 
+func TestBinarySearchOnBigSliceSuccess(t *testing.T) {
+	binarySearchResult := binarySearch([]int{0, 1, 3, 5, 7, 9, 13, 23, 25, 32}, 23)
+	assert.Equal(t, 7, binarySearchResult)
+}
+
 func TestBinarySearchFail(t *testing.T) {
 	binarySearchResult := binarySearch([]int{0, 1, 3, 4}, 2)
 	assert.Equal(t, -1, binarySearchResult)
